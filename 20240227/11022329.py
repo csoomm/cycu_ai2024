293c7a13c35493e10cf8e41787c26724ace215e8
周冠嘉 <160596388+csoomm@users.noreply.github.com>
2024-02-27 20:44:41 +0800

    Update 11022329.py

--- 20240227/11022329.py
+++ 20240227/11022329.py
@@ -1,15 +1,24 @@
-print("Hello World")
-
-#https://news.pts.org.tw/xml/newsfeed.xml 
-
+#https://news.pts.org.tw/xml/newsfeed.xml
 import feedparser
 
-def print_news_titles(rss_url):
+def print_news_titles_and_content(rss_url):
     feed = feedparser.parse(rss_url)
     for newsitem in feed['items']:
         print(newsitem['title'])
+        print(newsitem['summary'])
+        print("------")
 
 # RSS feed URL
 rss_url = "https://news.pts.org.tw/xml/newsfeed.xml"
 
-print_news_titles(rss_url)
+print_news_titles_and_content(rss_url)
+
+#幫我把帶有YouBike的新聞標題和內文印出來
+if __name__ == "__main__":
+    rss_url = "https://news.pts.org.tw/xml/newsfeed.xml"
+    feed = feedparser.parse(rss_url)
+    for newsitem in feed['items']:
+        if "YouBike" in newsitem['title']:
+            print(newsitem['title'])
+            print(newsitem['summary'])
+            print("------")
