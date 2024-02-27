53cc89b5fb9d9d6c6d1e44a75262d62ad45b8e2e
周冠嘉 <160596388+csoomm@users.noreply.github.com>
2024-02-27 20:48:08 +0800

    Update 11022329.py

--- 20240227/11022329.py
+++ 20240227/11022329.py
@@ -22,3 +22,22 @@ if __name__ == "__main__":
             print(newsitem['title'])
             print(newsitem['summary'])
             print("------")
+
+#幫我製成excel檔，檔名為0227作業，並儲存在桌面
+if __name__ == "__main__":
+    rss_url = "https://news.pts.org.tw/xml/newsfeed.xml"
+    feed = feedparser.parse(rss_url)
+    for newsitem in feed['items']:
+        if "YouBike" in newsitem['title']:
+            print(newsitem['title'])
+            print(newsitem['summary'])
+            print("------")
+            import os
+            import xlsxwriter
+            desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+            workbook = xlsxwriter.Workbook(desktop + '/0227作業.xlsx')
+            worksheet = workbook.add_worksheet()
+            worksheet.write('A1', newsitem['title'])
+            worksheet.write('A2', newsitem['summary'])
+            workbook.close()
+            
